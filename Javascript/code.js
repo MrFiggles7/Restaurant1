e78a1bbcaa87d812e83cf599c536fe501548a4f7
MrFiggles7 <rdjohnson7478@gmail.com>
2021-06-16 13:21:07 -0500

    Moved page transitions to CSS only. Added Anchor tags for menu pages deprecated JS code

--- Javascript/code.js
+++ Javascript/code.js
@@ -2,33 +2,25 @@ $(document).ready(
     function () {
 
         //event handlers
-        $('#menu-options__eat').click(function () {
-            $('.menu-options__page--eat').css("transform", "translateX(0)");
+        $('.menu-options__eat').click(function () {
+           /* $('.menu-options__page--eat').css("transform", "translateX(0)");
             $('.menu-options__page--eat').css("pointer-events", "all");
             $('.menu-options__page--eat').css("position", "relative");
-
+*/
             $('.header').css("position", "absolute");
             $('.header').css("width", "100%");
             $('.header').css("top", "0");
             $('.header').css("left", "0");
         });
 
-        $('#menu-options__drink').click(function () {
-            $('.menu-options__page--drink').css("transform", "translateX(0)");
-            $('.menu-options__page--drink').css("pointer-events", "all");
-            $('.menu-options__page--drink').css("position", "relative");
-
+        $('.menu-options__drink').click(function () {
             $('.header').css("position", "absolute");
             $('.header').css("width", "100%");
             $('.header').css("top", "0");
             $('.header').css("left", "0");
         });
 
-        $('#menu-options__visit').click(function () {
-            $('.menu-options__page--visit').css("transform", "translateX(0)");
-            $('.menu-options__page--visit').css("pointer-events", "all");
-            $('.menu-options__page--visit').css("position", "relative");
-
+        $('.menu-options__visit').click(function () {
             $('.header').css("position", "absolute");
             $('.header').css("width", "100%");
             $('.header').css("top", "0");
@@ -36,9 +28,9 @@ $(document).ready(
         });
 
         $('.topper__logo').click(function () {
-            $('.menu-options__page').css("transform", "translateX(-225rem)");
+           /* $('.menu-options__page').css("transform", "translateX(-225rem)");
             $('.menu-options__page').css("pointer-events", "none");
-            $('.menu-options__page').css("position", "absolute");
+            $('.menu-options__page').css("position", "absolute");*/
 
             $('.header').css("position", "relative");
         });
