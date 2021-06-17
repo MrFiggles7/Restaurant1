db4dc5368ea41d897e8c03d408c4d1e40a3f3aa9
MrFiggles7 <rdjohnson7478@gmail.com>
2021-06-17 14:15:20 -0500

    Reformatted html: moved main sections into header; now sibling of header div.
    converted navtarget checkboxes to :target pseudo class
    
    fixed bugs in main navigation; back button and refresh now do what they're supposed to

--- Javascript/code.js
+++ Javascript/code.js
@@ -1,39 +1,129 @@
 $(document).ready(
     function () {
 
+
+
+        var checkboxes = ['#menu-options--checkbox__eat',
+            '#menu-options--checkbox__home', '#menu-options--checkbox__drink',
+            '#menu-options--checkbox__visit'];
+
+        for(var i = 0; i < checkboxes.length-1; i++){
+            $(checkboxes[i]).prop('checked', false);
+        }
+
+        for(var i = 0; i < checkboxes.length-1; i++){
+            if(localStorage.getItem('checked') == checkboxes[i]){
+                $(localStorage.getItem('checked')).prop('checked', true);
+            }
+            else{
+                $(checkboxes[i]).prop('checked', false);
+            }
+        }
+
         //event handlers
         $('.menu-options__eat').click(function () {
-           /* $('.menu-options__page--eat').css("transform", "translateX(0)");
-            $('.menu-options__page--eat').css("pointer-events", "all");
-            $('.menu-options__page--eat').css("position", "relative");
-*/
-            $('.header').css("position", "absolute");
-            $('.header').css("width", "100%");
-            $('.header').css("top", "0");
-            $('.header').css("left", "0");
+            //$('.menu-options__page--eat').css("transform", "translateX(0)");
+            //$('.menu-options__page--eat').css("pointer-events", "all");
+            //$('.menu-options__page--eat').css("position", "relative");
+
+            //$('.header').css("position", "absolute");
+            //$('.header').css("width", "100%");
+            //$('.header').css("top", "0");
+            //$('.header').css("left", "0");
+
+            /*$('#menu-options--checkbox__eat').prop('checked', true);
+            $('#menu-options--checkbox__home').prop('checked', false);
+            $('#menu-options--checkbox__drink').prop('checked', false);
+            $('#menu-options--checkbox__visit').prop('checked', false);*/
+
+            localStorage.setItem('checked', '#menu-options--checkbox__eat');
+
+            for(var i = 0; i < checkboxes.length-1; i++){
+                if(localStorage.getItem('checked') == checkboxes[i]){
+                    $(localStorage.getItem('checked')).prop('checked', true);
+                }
+                else{
+                    $(checkboxes[i]).prop('checked', false);
+                }
+            }
         });
 
         $('.menu-options__drink').click(function () {
-            $('.header').css("position", "absolute");
-            $('.header').css("width", "100%");
-            $('.header').css("top", "0");
-            $('.header').css("left", "0");
+            //$('.menu-options__page--drink').css("transform", "translateX(0)");
+            //$('.menu-options__page--drink').css("pointer-events", "all");
+            //$('.menu-options__page--drink').css("position", "relative");
+
+            //$('.header').css("position", "absolute");
+            //$('.header').css("width", "100%");
+            //$('.header').css("top", "0");
+            //$('.header').css("left", "0");
+
+            $('#menu-options--checkbox__drink').prop('checked', true);
+            $('#menu-options--checkbox__home').prop('checked', false);
+            $('#menu-options--checkbox__eat').prop('checked', false);
+            $('#menu-options--checkbox__visit').prop('checked', false);
+            localStorage.setItem('checked', '#menu-options--checkbox__drink');
+
+            for(var i = 0; i < checkboxes.length-1; i++){
+                if(localStorage.getItem('checked') == checkboxes[i]){
+                    $(localStorage.getItem('checked')).prop('checked', true);
+                }
+                else{
+                    $(checkboxes[i]).prop('checked', false);
+                }
+            }
         });
 
         $('.menu-options__visit').click(function () {
-            $('.header').css("position", "absolute");
-            $('.header').css("width", "100%");
-            $('.header').css("top", "0");
-            $('.header').css("left", "0");
+            //$('.menu-options__page--visit').css("transform", "translateX(0)");
+            //$('.menu-options__page--visit').css("pointer-events", "all");
+            //$('.menu-options__page--visit').css("position", "relative");
+
+            //$('.header').css("position", "absolute");
+            //$('.header').css("width", "100%");
+            //$('.header').css("top", "0");
+            //$('.header').css("left", "0");
+
+            $('#menu-options--checkbox__visit').prop('checked', true);
+            $('#menu-options--checkbox__home').prop('checked', false);
+            $('#menu-options--checkbox__eat').prop('checked', false);
+            $('#menu-options--checkbox__drink').prop('checked', false);
+            localStorage.setItem('checked', '#menu-options--checkbox__visit');
+
+            for(var i = 0; i < checkboxes.length-1; i++){
+                if(localStorage.getItem('checked') == checkboxes[i]){
+                    $(localStorage.getItem('checked')).prop('checked', true);
+                }
+                else{
+                    $(checkboxes[i]).prop('checked', false);
+                }
+            }
         });
+
 
         $('.topper__logo').click(function () {
-           /* $('.menu-options__page').css("transform", "translateX(-225rem)");
-            $('.menu-options__page').css("pointer-events", "none");
-            $('.menu-options__page').css("position", "absolute");*/
+            //$('.menu-options__page').css("transform", "translateX(-225rem)");
+            //$('.menu-options__page').css("pointer-events", "none");
+            //$('.menu-options__page').css("position", "absolute");
 
-            $('.header').css("position", "relative");
+            //$('.header').css("position", "relative");
+            $('#menu-options--checkbox__home').prop('checked', true);
+            $('#menu-options--checkbox__eat').prop('checked', false);
+            $('#menu-options--checkbox__drink').prop('checked', false);
+            $('#menu-options--checkbox__visit').prop('checked', false);
+            localStorage.setItem('checked', '#menu-options--checkbox__home');
+
+            for(var i = 0; i < checkboxes.length-1; i++){
+                if(localStorage.getItem('checked') == checkboxes[i]){
+                    $(localStorage.getItem('checked')).prop('checked', true);
+                }
+                else{
+                    $(checkboxes[i]).prop('checked', false);
+                }
+            }
         });
+
+
         //other functions
     }
 );
