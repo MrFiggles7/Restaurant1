b67ff5cd807235552443026710ebca5dee94de06
MrFiggles7 <rdjohnson7478@gmail.com>
2021-06-17 14:37:40 -0500

    deprecated JS code commented out checkbox logic

--- Javascript/code.js
+++ Javascript/code.js
@@ -31,21 +31,21 @@ $(document).ready(
             //$('.header').css("top", "0");
             //$('.header').css("left", "0");
 
-            /*$('#menu-options--checkbox__eat').prop('checked', true);
+            //$('#menu-options--checkbox__eat').prop('checked', true);
             $('#menu-options--checkbox__home').prop('checked', false);
-            $('#menu-options--checkbox__drink').prop('checked', false);
-            $('#menu-options--checkbox__visit').prop('checked', false);*/
+            //$('#menu-options--checkbox__drink').prop('checked', false);
+            //$('#menu-options--checkbox__visit').prop('checked', false);
 
             localStorage.setItem('checked', '#menu-options--checkbox__eat');
 
-            for(var i = 0; i < checkboxes.length-1; i++){
+            /*for(var i = 0; i < checkboxes.length-1; i++){
                 if(localStorage.getItem('checked') == checkboxes[i]){
                     $(localStorage.getItem('checked')).prop('checked', true);
                 }
                 else{
                     $(checkboxes[i]).prop('checked', false);
                 }
-            }
+            }*/
         });
 
         $('.menu-options__drink').click(function () {
@@ -58,20 +58,21 @@ $(document).ready(
             //$('.header').css("top", "0");
             //$('.header').css("left", "0");
 
-            $('#menu-options--checkbox__drink').prop('checked', true);
+            //$('#menu-options--checkbox__drink').prop('checked', true);
             $('#menu-options--checkbox__home').prop('checked', false);
-            $('#menu-options--checkbox__eat').prop('checked', false);
-            $('#menu-options--checkbox__visit').prop('checked', false);
+            //$('#menu-options--checkbox__eat').prop('checked', false);
+            //$('#menu-options--checkbox__visit').prop('checked', false);
+
             localStorage.setItem('checked', '#menu-options--checkbox__drink');
 
-            for(var i = 0; i < checkboxes.length-1; i++){
+            /*for(var i = 0; i < checkboxes.length-1; i++){
                 if(localStorage.getItem('checked') == checkboxes[i]){
                     $(localStorage.getItem('checked')).prop('checked', true);
                 }
                 else{
                     $(checkboxes[i]).prop('checked', false);
                 }
-            }
+            }*/
         });
 
         $('.menu-options__visit').click(function () {
@@ -84,20 +85,21 @@ $(document).ready(
             //$('.header').css("top", "0");
             //$('.header').css("left", "0");
 
-            $('#menu-options--checkbox__visit').prop('checked', true);
+            //$('#menu-options--checkbox__visit').prop('checked', true);
             $('#menu-options--checkbox__home').prop('checked', false);
-            $('#menu-options--checkbox__eat').prop('checked', false);
-            $('#menu-options--checkbox__drink').prop('checked', false);
+            //$('#menu-options--checkbox__eat').prop('checked', false);
+            //$('#menu-options--checkbox__drink').prop('checked', false);
+
             localStorage.setItem('checked', '#menu-options--checkbox__visit');
 
-            for(var i = 0; i < checkboxes.length-1; i++){
+            /*for(var i = 0; i < checkboxes.length-1; i++){
                 if(localStorage.getItem('checked') == checkboxes[i]){
                     $(localStorage.getItem('checked')).prop('checked', true);
                 }
                 else{
                     $(checkboxes[i]).prop('checked', false);
                 }
-            }
+            }*/
         });
 
 
@@ -107,10 +109,12 @@ $(document).ready(
             //$('.menu-options__page').css("position", "absolute");
 
             //$('.header').css("position", "relative");
-            $('#menu-options--checkbox__home').prop('checked', true);
+
+            /*$('#menu-options--checkbox__home').prop('checked', true);
             $('#menu-options--checkbox__eat').prop('checked', false);
             $('#menu-options--checkbox__drink').prop('checked', false);
-            $('#menu-options--checkbox__visit').prop('checked', false);
+            $('#menu-options--checkbox__visit').prop('checked', false);*/
+
             localStorage.setItem('checked', '#menu-options--checkbox__home');
 
             for(var i = 0; i < checkboxes.length-1; i++){
